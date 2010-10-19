efbbc23f8c751ce3c24eb97b8570df90a8aba7e2
kensnyder <kendsnyder@gmail.com>
2010-10-19 16:07:28 -0600

    Initial commit of source.

---
+++ src/diskStorage.js
@@ -0,0 +1,122 @@
+/**
+ * diskStorage.js
+ * Wrapper for localStorage that also stores non-string data by serializing via JSON and
+ *   avoids name collisions with other libraries by prefixing keys with a control character
+ * Copyright (c) 2010 Ken Snyder under the MIT license: http://www.opensource.org/licenses/mit-license.html
+ *
+ * NOTE: This library isn't for everybody. It does not support IE6 or IE7
+ * It does support any browser that supports localStorage including
+ *		IE8+, FF 3.5+, Safari 4+, Chrome 4.0+, Opera 10.50+, iPhone 2.0+, Android 2.0+
+ * See http://diveintohtml5.org/storage.html
+ *
+ * @usage
+ * diskStorage.isSupported(); // true
+ *
+ * diskStorage.setItem("prop1", "myString");
+ * diskStorage.length; // 1
+ * diskStorage.getItem("prop1"); // "myString"
+ *
+ * diskStorage.setItem("prop2", 5);
+ * diskStorage.getItem("prop2"); // 5
+ *
+ * diskStorage.setItem("prop3", {a: "alpha"});
+ * diskStorage.getItem("prop3"); // {a: "alpha"}
+ *
+ * // close browser and visit another time: our values persist!
+ * diskStorage.removeItem("prop3");
+ * diskStorage.length; // 2
+ *
+ * diskStorage.clear();
+ * diskStorage.length = 0;
+ *
+ * diskStorage.subscribe(myFunction); // myFunction will be triggerred on setItem and removeItem
+ */
+(function(global) {
+
+	function DiskStorage() {
+		this.length = window.localStorage.length;
+		this.subscribers = [];
+	}
+
+	DiskStorage.isSupported = function isSupported() {
+		// from http://diveintohtml5.org/storage.html
+		try {
+			return 'localStorage' in window && window['localStorage'] !== null;
+		} catch (e) {
+			return false;
+		}
+	};
+
+	DiskStorage.prototype.setItem = function setItem(key, value) {
+		// if not a string, serialize to JSON
+		value = Object.prototype.toString.call(value) == '[object String]' ? value : '\u0002' + JSON.stringify(value);
+		if (this.subscribers.length > 0) {
+			oldValue = this.getItem(key);
+			this.notify(key, oldValue, value);
+		}
+		// prefix with to avoid collisions with other libs
+		window.localStorage.setItem('\u0001' + key, value);
+		this.length = window.localStorage.length;
+		return this;
+	};
+
+	DiskStorage.prototype.getItem = function getItem(key) {
+		// prefix with '\t' to avoid collisions such as 'toString'
+		var value = window.localStorage.getItem('\t' + key, value);
+		if (value.charAt(0) == '\u0002') {
+			// if prefixed with our special char, unserialize JSON
+			value = JSON.parse(value.slice(1));
+		}
+		return value;
+	};
+
+	DiskStorage.prototype.removeItem = function removeItem(key) {
+		if (this.subscribers.length > 0) {
+			oldValue = this.getItem(key);
+			this.notify(key, oldValue, undefined);
+		}
+		// prefix with '\t' to avoid collisions such as 'toString'
+		window.localStorage.removeItem('\u0001' + key);
+		this.length = window.localStorage.length;
+		return this;
+	};
+
+	DiskStorage.prototype.clear = function clear() {
+		// prefix with '\t' to avoid collisions such as 'toString'
+		window.localStorage.clear();
+		this.length = 0;
+		return this;
+	};
+
+	DiskStorage.prototype.subscribe = function subscribe(callback) {
+		this.subscribers.push(callback);
+		return this;
+	};
+
+	DiskStorage.prototype.subscribe = function unsubscribe(callback) {
+		var newlist = [], i = 0, fn;
+		while ((fn = this.subscribers[i++])) {
+			if (fn !== callback) {
+				newlist.push(fn);
+			}
+		}
+		this.subscribers = newlist;
+		return this;
+	};
+
+	DiskStorage.prototype.notify = function notify(key, newValue, oldValue) {
+		var i = 0, fn;
+		while ((fn = this.subscribers[i++])) {
+			fn({timestamp: +new Date, target: this, key: key, oldValue: oldValue, newValue: newValue, url: window.location.href});
+		}
+		return this;
+	};
+
+	global.diskStorage = new DiskStorage;
+
+	// clean up memory for IE8
+	var isSupported = null,
+		setItem = null, getItem = null, removeItem = null, clear = null,
+		subscribe = null, unsubscribe = null, notify = null;
+
+})(this);
